8c3bee5681f788905e6ec602b7e73a9f27ec49e8
harvey251 <33844174+harvey251@users.noreply.github.com>
2022-07-02 22:20:06 +0100

    Add decorator for get request

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,22 @@
+"""
+Configuration for nox.
+
+use `nox list` for whats available
+"""
+
 import nox
 
 
-@nox.session(python=["3.9", "3.10"])
+@nox.session(
+    python=[
+        "3.9",
+    ]
+)
 def run_tests(session: nox.Session) -> None:
+    """
+    Run pytests against 3.9, the most current aws lambda.
+
+    https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html
+    """
     session.install("pytest")
     session.run("pytest")
