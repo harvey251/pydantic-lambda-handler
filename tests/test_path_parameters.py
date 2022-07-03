6bb81a3c558d39563e9faa2cb9428af2cd90ddf2
harvey251 <33844174+harvey251@users.noreply.github.com>
2022-07-03 19:29:29 +0100

    First steps (#6)

---
+++ tests/test_path_parameters.py
@@ -0,0 +1,51 @@
+from enum import Enum
+
+from pydantic_lambda_handler.main import PydanticLambdaHander
+
+app = PydanticLambdaHander()
+
+
+@app.get("/items/{item_id}")
+def handler(item_id):
+    return {"item_id": item_id}
+
+
+def test_path_parameters_without_typehint():
+    event = {"pathParameters": {"item_id": "1"}}
+    response = handler(event, None)
+    assert response["statusCode"] == 200
+    assert response["body"] == {"item_id": "1"}
+
+
+@app.get("/items/{item_id}")
+def handler_with_type_hint(item_id: int):
+    return {"item_id": item_id}
+
+
+def test_path_parameters_with_typehint():
+    event = {"pathParameters": {"item_id": "1"}}
+    response = handler_with_type_hint(event, None)
+    assert response["statusCode"] == 200
+    assert response["body"] == {"item_id": 1}
+
+
+class Animals(str, Enum):
+    dog = "dog"
+
+
+@app.get("/items/{item_id}")
+def handler_with_enum_type_hint(item_id: Animals):
+    return {"item_id": item_id}
+
+
+def test_path_parameters_with_enum_typehint():
+    event = {"pathParameters": {"item_id": "dog"}}
+    response = handler_with_enum_type_hint(event, None)
+    assert response["statusCode"] == 200
+    assert response["body"] == {"item_id": "dog"}
+
+
+def test_path_parameters_with_enum_typehint_typeerror():
+    event = {"pathParameters": {"item_id": "cat"}}
+    response = handler_with_enum_type_hint(event, None)
+    assert response["statusCode"] == 422
--- tests/test_response.py
+++ tests/test_response.py
@@ -3,12 +3,15 @@ from pydantic_lambda_handler.main import PydanticLambdaHander
 app = PydanticLambdaHander()
 
 
-@app.get("/index")
+@app.get("/")
 def hello_handler():
     return {"message": "Hello World"}
 
 
-def test_root():
+def test_response():
+    """
+    test that the message is returned in the body
+    """
     response = hello_handler({}, None)
     assert response["statusCode"] == 200
     assert response["body"] == {"message": "Hello World"}
