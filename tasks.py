d13e5c9b92f8b964a9cfe6c253341737c6f3b349
Mark Harvey <inspiredharvey@gmail.com>
2022-07-31 13:23:43 +0100

    fix: remove unused imports

--- tasks.py
+++ tasks.py
@@ -1,29 +1,22 @@
 import json
-import os
 import shutil
 import subprocess
 import sys
 from pathlib import Path
-from shutil import copy2
-from tempfile import SpooledTemporaryFile, TemporaryDirectory, mkdtemp
 
 import requests
 from invoke import task
 
-# from aws_cdk.aws_iam import Policy, PolicyDocument, PolicyStatement, Effect
 from pydantic_lambda_handler.gen_open_api_inspect import gen_open_api_inspect
 
 
 @task
 def build_and_deploy(c):
     root = Path(__name__).parent
-    # src = root.joinpath("src/pydantic_lambda_handler")
     requirements_dir = root.joinpath("demo_app_requirements")
     requirements_dir.mkdir(parents=True, exist_ok=True)
     demo_app_dir = root.joinpath("demo_app")
     shutil.rmtree(requirements_dir)
-    # shutil.copytree(src, requirements_dir, symlinks=False, ignore=None, copy_function=copy2, ignore_dangling_symlinks=False,
-    #                 dirs_exist_ok=True)
 
     # https://aws.amazon.com/premiumsupport/knowledge-center/lambda-python-package-compatible/
     # pip install \
