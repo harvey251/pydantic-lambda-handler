1cb8651e67a116ff4e164bda94168dbd64ac620e
Mark Harvey <inspiredharvey@gmail.com>
2022-08-15 21:37:32 +0100

    fix: add a space

--- demo_app/demo_app/handler_app.py
+++
@@ -1,3 +0,0 @@
-from pydantic_lambda_handler.main import PydanticLambdaHandler
-
-plh = PydanticLambdaHandler(title="PydanticLambdaHandler")
--- src/pydantic_lambda_handler/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Pydantic lambda handler modelled on FastAPI
-"""
-__version__ = "0.6.1"
