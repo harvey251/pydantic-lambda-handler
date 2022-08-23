34ab7fc8e1ba8f5b4f53643b7fea2acd4c9d93d2
harvey251 <33844174+harvey251@users.noreply.github.com>
2022-08-23 21:30:13 +0100

    fix: Cli gen open api spec (#46)
    * fix: remove import
    
    * fix:add args

--- src/pydantic_lambda_handler/gen_open_api_inspect.py
+++ src/pydantic_lambda_handler/gen_open_api_inspect.py
@@ -1,3 +1,4 @@
+import json
 import os
 from ast import Import, ImportFrom, parse, walk
 from copy import deepcopy
@@ -68,3 +69,28 @@ def gen_open_api_inspect(dir_path: Path):
             next(h for h in app._hooks if issubclass(h, MockRequests)).testing_stuff,  # type: ignore
         )
     raise ValueError("App not found")
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("app_dir")
+    parser.add_argument("output_file_path")
+    args = parser.parse_args()
+
+    app_dir = args.app_dir
+    output_file_path = args.output_file_path
+
+    app_dir_path = Path(app_dir)
+    app_dir_path.exists()
+
+    schema, *_ = gen_open_api_inspect(app_dir_path)
+
+    output_file_path = Path(output_file_path)
+    output_file_path.parent.exists()
+
+    schema = json.loads(schema)
+
+    with output_file_path.open("w") as f:
+        json.dump(schema, f, indent=4)
