0b98777ec96e396a4fb99945df1d7f03777e4371
Mark Harvey <inspiredharvey@gmail.com>
2022-08-30 23:38:56 +0100

    ci: add code cov

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,8 @@ use `nox list` for whats available
 import nox
 
 
-@nox.session(python=["3.9", "3.10"])
+# @nox.session(python=["3.9", "3.10"])
+@nox.session(python=["3.9"])
 def run_tests(session: nox.Session) -> None:
     """
     Run pytests against 3.9, the most current aws lambda.
@@ -15,4 +16,4 @@ def run_tests(session: nox.Session) -> None:
     https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html
     """
     session.install(".[tests]")
-    session.run("pytest --cov=src")
+    session.run("pytest")
