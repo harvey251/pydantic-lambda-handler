da5c5d35fa5e50f85872846cc7b0b070373b6559
Mark Harvey <inspiredharvey@gmail.com>
2022-09-21 19:59:40 +0100

    CLAW

--- demo_app/src/subfolder/response_model_handlers.py
+++ demo_app/src/subfolder/response_model_handlers.py
@@ -1,7 +1,10 @@
 from typing import Optional
 
+import geopandas
+import pandas as pd
 from handler_app import plh
 from pydantic import BaseModel
+from responses import GpkgFileResponse
 
 
 class FunModel(BaseModel):
@@ -26,3 +29,20 @@ def response_list():
 @plh.get("/list_response_model", response_model=ListFunModel)
 def response_list_model():
     return [{"item_name": "secret"}]
+
+
+@plh.get("/response_file", response_class=GpkgFileResponse)
+def response_model(secret):
+    df = pd.DataFrame(
+        {
+            "City": ["Buenos Aires", "Brasilia", "Santiago", "Bogota", "Caracas"],
+            "Country": ["Argentina", "Brazil", "Chile", "Colombia", "Venezuela"],
+            "Latitude": [-34.58, -15.78, -33.45, 4.60, 10.48],
+            "Longitude": [-58.66, -47.91, -70.66, -74.08, -66.86],
+        }
+    )
+
+    gdf = geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude))
+
+    file = gdf.to_file("package.gpkg", layer="countries", driver="GPKG")
+    return GpkgFileResponse(file)
