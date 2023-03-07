060bdb0176efbf430dbb9627ee2bee50c21fd3c0
Mark Harvey <inspiredharvey@gmail.com>
2023-03-07 10:35:06 +0000

    fix: add is_base_64_encoded=False to response

--- src/pydantic_lambda_handler/main.py
+++ src/pydantic_lambda_handler/main.py
@@ -150,6 +150,7 @@ class PydanticLambdaHandler:
                             response = BaseOutput(
                                 body=json.dumps({"detail": json.loads(e.json())}),
                                 status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+                                is_base_64_encoded=False,
                             )
                             return loads(response.json())
 
@@ -195,8 +196,7 @@ class PydanticLambdaHandler:
                                             }
                                         )
                                     response = BaseOutput(
-                                        body=body,
-                                        status_code=e_status_code,
+                                        body=body, status_code=e_status_code, is_base_64_encoded=False
                                     )
                                     return loads(response.json())
                         raise
@@ -208,9 +208,11 @@ class PydanticLambdaHandler:
                         body = response_model.parse_obj(body)
 
                     if hasattr(body, "json"):
-                        base_output = BaseOutput(body=body.json(), status_code=status_code)
+                        base_output = BaseOutput(body=body.json(), status_code=status_code, is_base_64_encoded=False)
                     else:
-                        base_output = BaseOutput(body=json.dumps(body), status_code=status_code)
+                        base_output = BaseOutput(
+                            body=json.dumps(body), status_code=status_code, is_base_64_encoded=False
+                        )
 
                     for hook in self._hooks:
                         body = hook.pre_return(base_output)
